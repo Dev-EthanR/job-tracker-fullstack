98316eb485caec7b35a3861ca1eca6647406fbd2
Ethan <ethanlegend7@gmail.com>
2026-02-23 13:55:34 +1100

    fix signout alignment issue

--- src/app/auth/components/SignOut.tsx
+++ src/app/auth/components/SignOut.tsx
@@ -5,7 +5,7 @@ export function SignOut({ style }: { style: string }) {
   return (
     <button
       onClick={() => signOut({ redirectTo: "/auth" })}
-      className={`text-left ${style} cursor-pointer`}
+      className={`text-center md:text-left ${style} cursor-pointer`}
     >
       Sign Out
     </button>
