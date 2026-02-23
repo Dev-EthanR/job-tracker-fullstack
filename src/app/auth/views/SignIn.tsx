21549593274f66c60ccf57cc6ac52418048106e8
Ethan <ethanlegend7@gmail.com>
2026-02-23 12:36:41 +1100

    add title to auth

--- src/app/auth/views/SignIn.tsx
+++ src/app/auth/views/SignIn.tsx
@@ -3,6 +3,7 @@ import SignInProvider from "../components/SignInProvider";
 export function SignIn() {
   return (
     <div className="flex flex-col items-center justify-center h-full gap-4">
+      <h1 className="text-4xl mb-6 font-medium">Continue to Job Tracker</h1>
       <SignInProvider
         text="Continue with Google"
         provider="google"
